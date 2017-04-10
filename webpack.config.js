49a3cfd5cbe0aa73da0be092defee54d3b6f1642
thiagolucio <thiagolucio@gmail.com>
2017-04-10 19:31:03 -0300

    add mudanças de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,48 @@ module.exports = {
     entry: './app/app.module.js',
     output: {
         path: './bin',
+        path: __dirname + '/dist',
         filename: 'app.bundle.js',
+		publicPath: '/dist/'
     },
+    	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
     module: {
         loaders: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        },
-        { test: /\.html$/, loader: "html" },
-        { test: /\.css$/, loader: "style!css" }
+       
+       
       ]
+        },
+    	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				loader:'babel-loader',
+				query: {
+					presets: ['es2015']
+				}
+            },
+            { test: /\.html$/, loader: "html" },
+            { test: /\.css$/, loader: "style!css" },
+			{
+				test: /\.(woff|woff2|ttf|svg|eot)$/,
+				loader: 'url-loader?limit=100000',
+			},
+			{
+				test: /\.(scss|sass)$/,
+				loader: ['style-loader', 'css-loader', 'sass-loader'],
+			}
+		]
     },
+    	devServer: {
+		// host: '0.0.0.0',
+		inline: true,
+		watchOptions: {
+			poll: true,
+			aggregateTimeout: 300
+		}
+	}    
     devtool: "#inline-source-map"
 }
